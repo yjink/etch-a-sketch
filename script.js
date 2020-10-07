3e2e470eef2a89cbfefddc438af54f759ab4746d
yjink <kweonyujin@gmail.com>
2020-10-07 19:28:47 -0400

    add css.css and script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+// the .etch buttons;
+let newCanvas = document.querySelector("#new");
+let clearCanvas = document.querySelector("#clear");
+// the color buttons
+
+// the frame
+let frame = document.querySelector("#frame");
+let canvas = document.querySelector("#canvas");
+let col; // height;
+let row; // width;
+
+// New!
+newCanvas.addEventListener("click", () => {
+    row = prompt("What is x: ?");
+    col = prompt("What is y: ?");
+    // be friendlier
+
+    if (row < 30 && col < 30) {
+        grid(row, col);
+    } else {
+        alert("This canvas is too big!");
+    }
+});
+
+// Clear 
+clearCanvas.addEventListener("click", () => {
+    clear();
+    grid(row, col);
+})
+
+// make grid
+function grid(row, col) {
+    clear();
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+
+            square.addEventListener('mousemove', event => {
+                square.style.backgroundColor = 'yellow';
+            })
+        }
+    }
+
+    // square could be smaller?
+    canvas.style.width = row * 22 + 'px';
+    canvas.style.height = col * 22 + 'px';
+    frame.style.width = ((row * 22) * 1.25) + 'px';
+    frame.style.height = ((col * 22) * 1.25) + 'px';
+    // too put buttons in^^
+}
+
+function clear() {
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+}
